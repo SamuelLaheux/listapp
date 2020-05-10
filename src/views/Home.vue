c4cb45c04e9c65bbf832a5f6e8af1b9d3395abbf
Samuel Laheux <samuel.laheux@novadiscovery.com>
2020-05-11 01:20:40 +0200

    feat: add a list of input checkboxes and input labels

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,15 @@
     <img alt="Vue logo" src="../assets/logo.png" width="5%" height="5%" />
     <HelloWorld msg="List App" />
     <BaseCheckbox value="Item 1" />
+
+    <input type="number" step="1" v-model="nb" placeholder="Number of items" />
+    <ul>
+      <li v-for="index in nbl" :key="index">
+        <b>{{ index }}</b>
+        <input type="checkbox" id="index" v-model="checkedList[index]" />
+        <input v-model="messageList[index]" placeholder="edit me" />
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -15,6 +24,18 @@ export default {
   components: {
     HelloWorld,
     BaseCheckbox
+  },
+  data() {
+    return {
+      nb: 2,
+      checkedList: [false, false],
+      messageList: ['First', '']
+    }
+  },
+  computed: {
+    nbl: function () {
+      return Array.from(Array(parseInt(this.nb)).keys())
+    }
   }
 }
 </script>
