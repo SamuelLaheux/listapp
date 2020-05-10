9f8ae444568a9df4867010198f6ebb7962875ba3
Samuel Laheux <samuel.laheux@novadiscovery.com>
2020-05-10 21:56:19 +0200

    feat: add BaseCheckbox component

---
+++ src/components/BaseCheckbox.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <input type="checkbox" id="jack" value="value" v-model="checkedNames" />
+    <label for="value">{{ value }}</label>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component
+export default class BaseCheckbox extends Vue {
+  @Prop({ type: String }) private value!: string
+  @Prop({ type: String }) private checkedNames: string[] = []
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <img alt="Vue logo" src="../assets/logo.png" width="5%" height="5%" />
+    <HelloWorld msg="List App" />
+    <BaseCheckbox value="Item 1" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import HelloWorld from '@/components/HelloWorld.vue'
+import BaseCheckbox from '@/components/BaseCheckbox.vue'
 
 export default {
-  name: "Home",
+  name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
+    BaseCheckbox
   }
-};
+}
 </script>
