c71cf7e15f90ca87ba3a1a1a2613150da70f9683
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-10 23:22:17 +0000

    Deploy SamuelLaheux/listapp to github.com/SamuelLaheux/listapp.git:gh-pages

---
+++ precache-manifest.870513fdc6465190ac6a5ba5635a4079.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "773ff2cbc28a89a7dc3b",
+    "url": "/listapp/css/app.1daf180d.css"
+  },
+  {
+    "revision": "4d1b1a13fc2274b6f994a58cd8b42cd0",
+    "url": "/listapp/img/logo.4d1b1a13.png"
+  },
+  {
+    "revision": "e10c5d405943888772e1553907e70617",
+    "url": "/listapp/index.html"
+  },
+  {
+    "revision": "f5f7c0e4a3a6ed1c98aa",
+    "url": "/listapp/js/about.70736840.js"
+  },
+  {
+    "revision": "773ff2cbc28a89a7dc3b",
+    "url": "/listapp/js/app.f9d12925.js"
+  },
+  {
+    "revision": "d3cb8bd895765ef4c0cd",
+    "url": "/listapp/js/chunk-vendors.4e14920c.js"
+  },
+  {
+    "revision": "69d89260fa1d0893485068ad8c7dd889",
+    "url": "/listapp/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/listapp/robots.txt"
+  }
+]);
